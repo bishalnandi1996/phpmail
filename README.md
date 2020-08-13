f3748a7ec4dcaccc2353f95e87db95f545694619
BISHAL <bishalnandi1996@gmail.com>
2020-08-13 13:40:56 +0530

    Visual Update

--- README.md
+++ README.md
@@ -8,5 +8,8 @@ Send mail with attachment using PHP script
 - PHP 7+
 - Any text editor for customization
 
+#### About
+This script can send mail to any specified mail address from a normal html form using core php. No external library is needed. To specify the receiver mail address open the send_mail.php script with any text editor and replace the mail id with the receiver's mail.
+
 **For any techncal issue contact**
-[BISHAL](https://www.linkedin.com/in/bishalnandi1996)
+ [BISHAL](https://www.linkedin.com/in/bishalnandi1996)
--- index.php
+++ index.php
@@ -9,31 +9,47 @@
 		echo "<script>alert('Mail has been sent successfully')</script>";
 	}
 ?>
+
+	<style>
+		.form-group {
+			margin-top: 15px;
+		}
+		
+		.frm-input {
+			border: 1px solid #a5a5a5;
+			border-radius: 0px;
+		}
+		
+		div {
+			margin-top: 5px;
+		}
+	</style>
+
 </head>
 
 <body style="overflow-x: hidden;">
 
-<form name="frmMail" method="post" action="send_mail.php" enctype="multipart/form-data">
+<form name="frmMail" method="post" action="send_mail.php" enctype="multipart/form-data" >
 	<div class="form-group">
 		<label>Email:</label>
 		<div>
-			<input type="email" name="frmEmail" required/>
+			<input type="email" class="frm-input" name="frmEmail" required/>
 		</div>
 	</div>
 	<div class="form-group">
 		<label>Message:</label>
 		<div>
-			<textarea name="frmMsg" rows="4" cols="30" required></textarea>
+			<textarea name="frmMsg" class="frm-input" rows="4" cols="30" required></textarea>
 		</div>
 	</div>
 	<div class="form-group">
-		<label>Upload zip file (Max size 10MB):</label>
+		<label>Upload zip file:</label>
 		<div>
-			<input type="file" name="frmZipFile" accept=".zip"  style="border: none;"/>
+			<input type="file" name="frmZipFile" class="frm-input" accept=".zip"  style="border: none;"/>
 		</div>
 	</div>
 	<div class="form-group">
-		<input type="submit" value="Send Mail" />
+		<input type="submit" class="frm-input" value="Send Mail" style="padding: 10px; background: #20c652; color: #ffffff;" />
 	</div>
 </form>
 
